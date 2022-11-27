675b01d86d0ce4a5d16e6941a69d882ce7448aa3
Mikita_Kobyzau <Mikita_Kobyzau@epam.com>
2022-11-27 12:05:00 +0100

    initial commit

---
+++ src/main/java/com/kobyzau/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kobyzau.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ok("""
+                                         {
+                                         "status": "OK"
+                                         }
+                                         """);
+    }
+}
